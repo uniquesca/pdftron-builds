4a342d0b8dba8a373ab10b5e12ee1af5cb5777a4
Andron <andron@uniques.ca>
2023-11-17 10:47:56 -0800

    Update: renewed license key

--- test/LicenseKey.php
+++ test/LicenseKey.php
@@ -5,7 +5,7 @@
 //---------------------------------------------------------------------------------------
 
 //Enter your key here. If you don't have it, please go to https://www.pdftron.com/pws/get-key to obtain a demo license or https://www.pdftron.com/form/contact-sales to obtain a production key.
-$LicenseKey = "Uniques Software Corporation (uniques.ca):OEM:Officio::L:AMS(20230928):1C773D101FD724D02313227860611FA5BF60D5154E541DF454F518A02ACA31F5C7";
+$LicenseKey = "Uniques Software Corporation:OEM:Officio::L:AMS(20240928):2C77FD101FD724D0D313227860611FA5BF60D5154E541DF454F518A02ACA31F5C7";
 if($LicenseKey == "YOUR_PDFTRON_LICENSE_KEY"){
     throw new Exception("Please enter your license key by replacing \"YOUR_PDFTRON_LICENSE_KEY\" that is assigned to the LicenseKey variable in Samples/LicenseKey/PHP/LicenseKey.php. If you do not have a license key, please go to https://www.pdftron.com/pws/get-key to obtain a demo license or https://www.pdftron.com/form/contact-sales to obtain a production key.");
 }
